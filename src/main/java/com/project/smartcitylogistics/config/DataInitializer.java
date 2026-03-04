8ef2cf332027a451a7c597593cede8d4a69a3d58
Siyabonga <sdeut291@gmail.com>
2026-03-04 17:39:26 +0200

    feat: add PostGIS spatial queries for proximity and KNN search

--- src/main/java/com/project/smartcitylogistics/config/DataInitializer.java
+++ src/main/java/com/project/smartcitylogistics/config/DataInitializer.java
@@ -4,20 +4,23 @@ import com.project.smartcitylogistics.entity.Courier;
 import com.project.smartcitylogistics.repository.CourierRepository;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Point;
 import org.locationtech.jts.geom.PrecisionModel;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Component
 public class DataInitializer implements CommandLineRunner {
 
-    private final CourierRepository repository;
+    private final CourierRepository courierRepository;
     // SRID 4326 is the standard for GPS (WGS84)
     private final GeometryFactory factory = new GeometryFactory(new PrecisionModel(), 4326);
 
     public DataInitializer(CourierRepository repository) {
-        this.repository = repository;
+        this.courierRepository = repository;
     }
 
     @Override
@@ -31,7 +34,17 @@ public class DataInitializer implements CommandLineRunner {
         // Coordinates for a test location (Longitude, Latitude)
         courier.setCurrentLocation(factory.createPoint(new Coordinate(28.2293, -25.7479)));
 
-        repository.save(courier);
+        courierRepository.save(courier);
         System.out.println(">>> Smart City Courier Saved with GPS Coordinates!");
+
+        // Defining a point
+        GeometryFactory factory = new GeometryFactory(new PrecisionModel(), 4326);
+        Point searchPoint = factory.createPoint(new Coordinate(28.22, -25.75));
+
+        // Search within 5km (5000 meters)
+        List<Courier> nearby = courierRepository.findNearbyCouriers(searchPoint, 5000);
+
+        System.out.println(">>> Found " + nearby.size() + " couriers nearby!");
+        nearby.forEach(c -> System.out.println(" - " + c.getName()));
     }
 }
